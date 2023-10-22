53bcb73f85a05931f252dcbf806c082d042ae1b9
momin morgan <mominmorgan10@gmail.com>
2023-10-22 11:44:24 -0700

    adding numberer

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -32,8 +32,8 @@ def python(text="is cool"):
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-     n = str(n)
-     return "{} is a number".format(n)
+    n = str(n)
+    return "{} is a number".format(n)
 
 
 if __name__ == '__main__':
