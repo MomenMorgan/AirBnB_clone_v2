c9cd2ada36e8dc441554a2b276d314362acf036b
momin morgan <mominmorgan10@gmail.com>
2023-10-22 12:29:11 -0700

    html fix

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -44,7 +44,7 @@ def number_template(n):
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def eve_odd(n):
-    return render_template("6-number_odd_or_even.py", n=n)
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == '__main__':
